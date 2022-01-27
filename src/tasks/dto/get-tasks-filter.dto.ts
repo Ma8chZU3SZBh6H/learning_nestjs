e9e96adf6fa9e308400ed6d5e777f657bedeffcd
Kristupas <monstertooth1@gmail.com>
2022-01-27 21:33:20 +0200

    task filter validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
 import {TaskStatus} from "../task.mode";
+import {IsEnum, IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class GetTasksFilterDto{
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
     search? : string;
 }
