39d79582745867355695367fbf30d0174bb4cb0a
Kristupas <monstertooth1@gmail.com>
2022-01-28 21:47:14 +0200

    created auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsersRepository} from "./users.repository";
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([UsersRepository])
   ],
-  providers: [AuthService]
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
