23d58490430bed22627179422c6feebab55e6f56
Kristupas <monstertooth1@gmail.com>
2022-01-28 22:22:29 +0200

    signup validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,13 @@
+import {IsString, MaxLength, MinLength} from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(24)
     username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(100)
     password: string;
 }
