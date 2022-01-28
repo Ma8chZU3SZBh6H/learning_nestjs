44c50167d292e8094abaa3f38fd7ac66cc6c9c69
Kristupas <monstertooth1@gmail.com>
2022-01-28 21:44:21 +0200

    created users model and repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsersRepository} from "./users.repository";
 
 @Module({
+  imports: [
+      TypeOrmModule.forFeature([UsersRepository])
+  ],
   providers: [AuthService]
 })
 export class AuthModule {}
