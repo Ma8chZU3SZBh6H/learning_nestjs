bf2204c4ce6addcef33c7e4ceea453ea5475d6b4
Kristupas <monstertooth1@gmail.com>
2022-01-28 23:27:42 +0200

    imported jtw modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsersRepository} from "./users.repository";
 import { AuthController } from './auth.controller';
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
   imports: [
+      PassportModule.register({defaultStrategy: 'jwt'}),
+      JwtModule.register({
+          secret: 'test',
+          signOptions: {
+              expiresIn: 3600
+          }
+      }),
       TypeOrmModule.forFeature([UsersRepository])
   ],
   providers: [AuthService],
